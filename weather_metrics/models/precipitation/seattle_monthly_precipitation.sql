19b1a16650fdf594ecaaa165eecc4decc599e298
Rushil Shah <rps1073@gmail.com>
2023-04-28 23:06:52 -0400

    cleanup

--- weather_metrics/models/precipitation/seattle_monthly_precipitation.sql
+++ weather_metrics/models/precipitation/seattle_monthly_precipitation.sql
@@ -2,15 +2,16 @@ WITH forecast_months AS (
     SELECT
         CONCAT(YEAR(forecast_date),MONTH(forecast_date)) AS forecast_month,
         forecast_date
-    FROM {{ ref('boston_daily_forecast') }}
-), boston_monthly_precipitation AS (
+    FROM {{ ref('seattle_daily_forecast') }}
+), seattle_monthly_precipitation AS (
     SELECT DISTINCT
-        fm.forecast_month,
+        YEAR(f.forecast_date) AS forecast_year,
+        MONTH(f.forecast_date) AS forecast_month,
         f.city,
         SUM(totalprecip_in) OVER(PARTITION BY forecast_month) total_monthly_precipitation
     FROM forecast_months fm
-    JOIN {{ ref('boston_daily_forecast') }} f
+    JOIN {{ ref('seattle_daily_forecast') }} f
     ON fm.forecast_date = f.forecast_date
 )
 SELECT *
-FROM boston_monthly_precipitation
+FROM seattle_monthly_precipitation
